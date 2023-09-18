d4c35c3d0d425012f7d96f097113ebb11d61acfe
zhangweiyu <zhangweiyu@adtiger.hk>
2023-09-18 13:31:19 +0800

    update code

--- 手写call/index.js
+++ 手写call/index.js
@@ -1,4 +1,19 @@
-Function.prototype.myCall = function (context) {
-  context.fn = this;
-  context.fn();
+Function.prototype.myCall = function (ctx, ...args) {
+  ctx = ctx === undefined || ctx === null ? globalThis : Object(ctx);
+  var fn = this;
+  var key = Symbol("temp");
+  Object.defineProperty(ctx, key, {
+    value: fn,
+    enumerable: false,
+  });
+  const result = ctx[key](...args);
+  delete ctx[key];
+  return result;
 };
+
+function method(a, b) {
+  console.log(a, b);
+  console.log(this);
+}
+
+method.myCall("aa", 1, 2);
